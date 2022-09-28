54ef508e635a4970c035dfe714a3fa594fa94b7e
KagemniKarimu <82295340+KagemniKarimu@users.noreply.github.com>
2022-09-28 07:23:39 -0500

    added html templates, revised mux

--- angacloud.go
+++ angacloud.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 var mu sync.Mutex
 var totalRequests int
 var startTime = time.Now()
+var default404 http.Handler = http.HandlerFunc(errorPage)
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/debug", debugInfo)
+	r.NotFoundHandler = default404
+	fmt.Print("anga.cloud is active!\n")
+	fmt.Print("http://localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
+
+func errorPage(w http.ResponseWriter, r *http.Request) {
+	increaseCount()
+	content, err := os.ReadFile("error.html")
+	if err != nil {
+		fmt.Printf("Error %s", err)
+	}
+	fmt.Fprint(w, string(content))
 }
 
 func increaseCount() {
@@ -29,7 +44,11 @@ func increaseCount() {
 func home(w http.ResponseWriter, r *http.Request) {
 	increaseCount()
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>HABARI GANI?</h1>")
+	content, err := os.ReadFile("index.html")
+	if err != nil {
+		fmt.Printf("Error %s", err)
+	}
+	fmt.Fprintf(w, string(content))
 }
 
 func debugInfo(w http.ResponseWriter, r *http.Request) {
@@ -40,6 +59,7 @@ func debugInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Requests: %d\n", totalRequests)
 	fmt.Fprintf(w, "Start Time: %q\n Uptime: %s\n", startTime, upTime)
 	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	// fmt.Fprintf(w, "Host OS:%s\n", )
 	for k, v := range r.Header {
 		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 	}
